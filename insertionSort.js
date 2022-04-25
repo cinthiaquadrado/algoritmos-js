9ffd1209aa876aa118b29720e674ac655fb99370
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-04-25 13:26:19 -0300

    Atualizando insertionSort.js para arrumar array

--- insertionSort.js
+++ insertionSort.js
@@ -1,1 +1,22 @@
+//inserindo elementos na posicao correta com relacao ao que vem antes dele
 
+const livros = require('./listaLivros');
+
+function insertionSort(lista){
+  
+  for (let atual = 0; atual < lista.length; atual++) {
+    let analise = atual; //movendo de item por item 
+    while (analise > 0 && lista[analise].preco < lista[analise - 1).preco) {
+      let itemAnalise = lista[analise];
+      let itemAnterior = lista[analise - 1];
+      
+      lista[analise] = itemAnterior
+      lista[analise - 1] = itemaAnalise
+      
+      analise-- //analisando e voltando ate o momento em que a condicao do while nao e mais verdadeira
+    }
+  }
+  console.log(lista);
+}
+
+insertionSort(livros); //lista arrumada por valor
